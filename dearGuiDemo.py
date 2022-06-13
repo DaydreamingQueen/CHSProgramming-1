d5afaee500febcaa3eafa27fa72ab8b96c1f7429
Samantha Vialpando <106619134+DaydreamingQueen@users.noreply.github.com>
2022-06-13 09:30:01 -0700

    Update dearGuiDemo.py

--- dearGuiDemo.py
+++ dearGuiDemo.py
@@ -1,14 +1,8 @@
 import dearpygui.dearpygui as dpg
+import dearpygui.demo as demo
 
 dpg.create_context()
-try: dpg.create_viewpoint(title='Custom Title', width=600, height=300)
-except: AttributeError
-
-with dpg.viewpoint(lable='Example Window'):
-    dpg.add_text('Hello, world!')
-    dpg.add_button(lable='Save')
-    dpg.add_input_text(lable='string', default_value='Quick brown fox')
-    dpg.add_slider_float(lable='float', default_value=0.273, max_value=1)
+dpg.create_viewport(title='demo screen',width=300,height=400)
 
 dpg.setup_dearpygui()
 dpg.show_viewpoint()
